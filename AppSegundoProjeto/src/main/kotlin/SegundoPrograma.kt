975f2bb89afd9bfdcc7e7c80b5a2b7b509ddbf04
Edilson <edilsonantonio0@gmail.com>
2023-02-09 21:29:11 -0300

    2 projeto

---
+++ AppSegundoProjeto/src/main/kotlin/SegundoPrograma.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+var variavelA = "Exemplo"
+var variavelB = "Variavel"
+
+ print("$variavelA\t$variavelB\n") // exemplo de variavel
+
+
+ print("Programação\nKotlin\n")    // (\n) usado para quebra linhas.
+ print("Programação\t\tKotlin")    // (\t\t) usado para dar espaços maiores entre as letras.
+
+}
