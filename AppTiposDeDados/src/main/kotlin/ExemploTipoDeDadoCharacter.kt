a46368a40e20924fdc649fb67dd59e56d7f0f4fd
Edilson <edilsonantonio0@gmail.com>
2023-02-16 21:49:10 -0300

    Exemplo tipo de dados Charavter

---
+++ AppTiposDeDados/src/main/kotlin/ExemploTipoDeDadoCharacter.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    //copliador sempre sabe o tipo de dado, ele converte par o melhor tipo de dado
+    val letraA = 'A'
+    val nome = "Edilson"
+
+    val letraB: Char='B'
+    val cidade: String = "olinda"
+
+    print("Letra A = $letraA\n")
+    print("Nome= $nome\n")
+    print("Letra B= $letraB\n")
+    print("Cidade= $cidade\n")
+
+
+}
